eb4a7e08af46a214f6ee515f2953ace49040ce7b
Gustavo Figueiredo Martines <gustavo.martines2003@outlook.com>
2025-03-07 22:22:19 -0300

    Foi feito uma box

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿
+Console.ForegroundColor = ConsoleColor.Red;
+
+Console.Write(@"
+   ______________________________
+ / \                             \.
+|   |               Jubileu      |.
+ \_ |                            |.
+    |     Quer                   |.
+    |                            |.
+    |                            |.
+    |                            |.
+    |                            |.
+    |           Pipoca           |.
+    |                            |.
+    |                            |.
+    |                            |.
+    |                            |.
+    |             ass:Jubileu    |.
+    |   _________________________|___
+    |  /                            /.
+    \_/____________________________/.");
+
+Console.ResetColor();
